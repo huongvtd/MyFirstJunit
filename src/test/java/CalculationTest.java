5edb89cf38f12c95219484f3da32c7b35b5b949a
huong <huongvtd@gmail.com>
2023-04-17 23:03:29 +0700

    041720231

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,18 +1,42 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculationTest {
-    @Test
-    public void addTest() {
-        CalculationDemo calculationDemo = new CalculationDemo();
-        double result = CalculationDemo.add(1.7, 8.3);
-        Assertions.assertEquals(10.2, result, "Addition is incorrect");
+    private CalculationDemo calculationDemo;
+
+    @BeforeAll
+    public static void setupClass () {
+        System.out.println("Set up Class");
+    }
+
+    @BeforeEach
+    public void setup (){
+        calculationDemo = new CalculationDemo();
+        System.out.println("Set up Test");
+    }
+
+    public static double [][] data(){
+               return new double[][] {{1,2,3}, {2,3,4}, {1.2,1.3,2.5}};
+    }
+
+    @ParameterizedTest
+    @MethodSource (value = "data")
+    public void addTest(double[] data) {
+        Assertions.assertEquals(data[2], CalculationDemo.add(data[0],data[1]), "Addition is incorrect");
     }
 
     @Test
     public void multiplyTest (){
-        CalculationDemo calculationDemo = new CalculationDemo();
         double result = CalculationDemo.multiply(3.5,1);
         Assertions.assertEquals(3.5, result, "Multiplication is not exact");
+    }
+
+    @AfterEach
+    public void teardown  () {
+        System.out.println("Tear down test");
+    }
+
+    @AfterAll
+    public static void teardownClass (){
+        System.out.println("Tear down Class");
     }
 }
